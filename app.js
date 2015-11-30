e759d09d46a8f1377def3346f1c0bdd78d663f24
Mathias Desloges <freakdev@gmail.com>
2015-11-30 16:12:00 +0100

    implement wait frame

--- app.js
+++ app.js
@@ -5,15 +5,16 @@ document.addEventListener('DOMContentLoaded', function () {
         "q1": {"type": 1, "text": "ça va?", "answers": {"a1": {"text": "oui", "goto": "e2"}, "a2": {"text": "non", "goto": "e3"}}},
         "e2": {"type": 0, "text": "j'en suis heureux", "goto": "e4"},
         "e4": {"type": 0, "text": "arf... je suis en retard...", "goto": "e5"},
-        "e5": {"type": 0, "text": "allons entrons au campus", "goto": "e6"},
+        "e5": {"type": 0, "text": "allons, entrons au campus", "goto": "e6"},
         "e6": {"type": 1, "text": "par derrière ou par devant?", "answers": {"a1": {"text": "derrière", "goto": "e7"}, "a2": {"text": "par devant", "goto": "e7"}}},
         "e3": {"type": 0, "text": "mince, il te faut un arrêt maladie", "goto": "e8"},
         "e8": {"type": 0, "text": "le plus simple serait de le faire avec toshop...", "goto": "e9"},
         "e9": {"type": 1, "text": "à moins que tu ne connaisse une medecin?", "answers": {"a1": {"text": "oui j'ai un medecin", "goto": "e10"}, "a2": {"text": "non, va pour toshop", "goto": "e11"}}},
         "e10": {"type": 0, "text": "je lui passe un coups de fil de suite!", "goto": "e12"},
         "e11": {"type": 0, "text": "faut espéré que ... soit la pour me faire ça...", "goto": "e5"},
-        "e12": {"type": 0, "text": "[... est au téléphone]", "goto": "e5"},
-        "e7": {"type": 0, "text": "bah... de toute façon il y a des agents des deux coté..."},
+        "e12": {"type": 0, "text": "[... est au téléphone]", "delay": 5000, "goto": "e13"},
+        "e13": {"type": 0, "text": "bon... il ne répond pas... tant pis", "goto": "e5"},
+        "e7": {"type": 0, "text": "bah... de toute façon il y a des agents des deux coté..."}
     }
 
     var current = "start";
@@ -22,27 +23,36 @@ document.addEventListener('DOMContentLoaded', function () {
         var div;
 
         current = key;
+
         div = document.createElement('div');
-        div.innerHTML = story[key].text;
+        div.innerHTML = story[current].text;
         document.body.appendChild(div);
-        if (story[key].type == 1) {
+        if (story[current].type == 1) {
             div = document.createElement('div');
             div.className += "question";
-            div.innerHTML = '<div class="ans" data-ans="a1">' + story[key].answers.a1.text + '</div><div class="ans" data-ans="a2">' + story[key].answers.a2.text + '</div><div class="clear"></div>';
+            div.innerHTML = '<div class="ans" data-ans="a1">' + story[current].answers.a1.text + '</div><div class="ans" data-ans="a2">' + story[current].answers.a2.text + '</div><div class="clear"></div>';
             div.addEventListener('click', onChoose);
             document.body.appendChild(div);
         } else {
-            if (story[key].goto) {
-                div = document.createElement('div');
-                div.innerHTML = '<div class="dots"><span class="dot"></span><span class="dot"></span><span class="dot"></span></div>';
-                div.className = "wait";
-                document.body.appendChild(div);
-                setTimeout(function () {
-                    document.body.removeChild(div);
-                    play(story[key].goto);
-                }, 1500 + (Math.random() * 1000));                
+            if (story[current].goto) {
+                if (story[current].delay) {
+                    setTimeout(next, story[current].delay);
+                } else {
+                    next();
+                }
             }
         }
+    }
+
+    function next() {
+        div = document.createElement('div');
+        div.innerHTML = '<div class="dots"><span class="dot"></span><span class="dot"></span><span class="dot"></span></div>';
+        div.className = "wait";
+        document.body.appendChild(div);
+        setTimeout(function () {
+            document.body.removeChild(div);
+            play(story[current].goto);
+        }, 1500 + (Math.random() * 1000));        
     }
 
     function onChoose(e) {
